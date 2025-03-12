74fa377e1b4a52fcb2ba08bd1dd8e74c312d306f
Zahid <btsbandatierrasagrada@gmail.com>
2025-03-11 22:37:48 -0600

    Calendario

---
+++ SalonDeBelleza/src/Citas/Calendario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SalonDeBelleza.src.Citas
+{
+    public class CalendarioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
