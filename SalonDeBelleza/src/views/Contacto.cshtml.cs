5a4c567cca6c14205be76b897ddeb415d834e70d
BandaTierraSagrada <btsbandatierrasagrada@gmail.com>
2025-04-23 20:03:27 -0600

    pagina de contacto y contacto css

---
+++ SalonDeBelleza/src/views/Contacto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SalonDeBelleza.src.views
+{
+    public class ContactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
