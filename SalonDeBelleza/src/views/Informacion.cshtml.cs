8d5814a95a5a0cdc98854261fcd7e9376080bb6a
BandaTierraSagrada <btsbandatierrasagrada@gmail.com>
2025-05-12 23:46:24 -0600

    boton logo bts, pagina informacion

---
+++ SalonDeBelleza/src/views/Informacion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SalonDeBelleza.src.views
+{
+    public class InformacionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
